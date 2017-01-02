fdbdedc344e203d5372938b461e1700d19c31bb7
Pedro Sales <pedrosalesrodriguez@gmail.com>
2017-01-02 01:29:06 -0300

    Toolbar controls and save/read from journal

--- activity.py
+++ activity.py
@@ -5,8 +5,11 @@ import sugargame
 import sugargame.canvas
 import gtk
 
+from gettext import gettext as _
+
 from sugar.activity import activity
 from sugar.graphics.toolbarbox import ToolbarBox
+from sugar.graphics.toolbutton import ToolButton
 from sugar.activity.widgets import ActivityToolbarButton
 from sugar.activity.widgets import StopButton
 
@@ -19,8 +22,8 @@ class PlanetsActivity(activity.Activity):
 
         # Change the following number to change max participants
         self.max_participants = 1
-        self.build_toolbar()
         self.activity = game.PlanetsGame()
+        self.build_toolbar()
         self._pygamecanvas = sugargame.canvas.PygameCanvas(self)
         self.set_canvas(self._pygamecanvas)
         self._pygamecanvas.grab_focus()
@@ -39,6 +42,38 @@ class PlanetsActivity(activity.Activity):
         activity_button = ActivityToolbarButton(self)
         toolbar_box.toolbar.insert(activity_button, -1)
         activity_button.show()
+
+        centerBut = ToolButton('view-radial')
+        centerBut.set_tooltip(_("Center view on planet"))
+        centerBut.connect('clicked', self.activity.center_button_pressed)
+        toolbar_box.toolbar.insert(centerBut, -1)
+        centerBut.show()
+
+        showBut = ToolButton('toolbar-view')
+        showBut.set_tooltip(_("Show selected planet"))
+        showBut.connect('clicked', self.activity.show_button_pressed)
+        toolbar_box.toolbar.insert(showBut, -1)
+        showBut.show()
+
+
+        delBut = ToolButton('edit-delete')
+        delBut.set_tooltip(_("Delete selected planet"))
+        delBut.connect('clicked', self.activity.del_button_pressed)
+        toolbar_box.toolbar.insert(delBut, -1)
+        delBut.show()
+
+
+        resBut = ToolButton('system-restart')
+        resBut.set_tooltip(_("Restart game"))
+        resBut.connect('clicked', self.activity.restart_button_pressed)
+        toolbar_box.toolbar.insert(resBut, -1)
+        resBut.show()
+
+        helpBut = ToolButton('toolbar-help')
+        helpBut.connect('clicked', self.activity.help_button_pressed)
+        toolbar_box.toolbar.insert(helpBut, -1)
+        helpBut.show()
+
         
         separator = gtk.SeparatorToolItem()
         separator.props.draw = False
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import pygame, math
+import pygame, math, pickle
 from pygame.locals import *
 from pygame import gfxdraw
 
@@ -15,7 +15,6 @@ class PlanetsGame():
     def __init__(self):
         self.planets = []
 
-
     def run(self):
         velScale = 1    #Velocity scale
         FPS = 60
@@ -27,26 +26,32 @@ class PlanetsGame():
         surface = pygame.display.get_surface()
 
         info = pygame.display.Info()
-        WIDTH = info.current_w
-        HEIGHT = info.current_h
+        self.WIDTH = info.current_w
+        self.HEIGHT = info.current_h
 
-        selectedPlanet = -1
+        self.selectedPlanet = -1
+        self.planetGrowing = False;
+        self.planetEditing = -1
+        self.showSelPlanet = False
+        self.showHelp = False
+        self.paused = False
+
+
+        self.pendingAction = ''
 
         font = pygame.font.SysFont(None, 20)
-        text = font.render('Press "h" for help', True, (255,255,255))
-        textRect = text.get_rect()
-        textRect.left = 0
-        textRect.bottom = HEIGHT
 
         helpString = """ 
-        HELP:
+        HELP
+        (Click help icon to close)
+
         Click and hold anywhere on the screen to create a planet.
         Click again to give speed to that planet, speed depends on the distance between
         the planet and the second click. Try placing a big planet with no speed and a smaller
         one with speed some distance from the first one, watch how they orbit!
 
         KEY SHORTCUTS:
-        h - Open this menu
+        h - Open/close this menu
         c - Center view on planet
         s - Show selected planet
         d - Delete selected planet
@@ -56,12 +61,8 @@ class PlanetsGame():
         using the left mouse button, use the right mouse button to give it speed relative to the
         selected planet. This is useful if you want to make a planet orbit another one in motion."""
 
-        planetGrowing = False;
-        planetEditing = -1
-        G = 10
 
-        showSelPlanet = False
-        showHelp = False
+        G = 10
 
         print (dt)
 
@@ -76,111 +77,120 @@ class PlanetsGame():
                 elif event.type == pygame.VIDEORESIZE:
                     pygame.display.set_mode(event.size, pygame.RESIZABLE)
                     info = pygame.display.Info()
-                    WIDTH = info.current_w
-                    HEIGHT = info.current_h
+                    self.WIDTH = info.current_w
+                    self.HEIGHT = info.current_h
 
                 elif event.type == MOUSEBUTTONDOWN:
-                    if planetGrowing == False and planetEditing == -1:   #Ready to draw new planet
+                    if self.planetGrowing == False and self.planetEditing == -1:   #Ready to draw new planet
                         newPlanet = {'center': (event.pos[0],event.pos[1]), 'radius': 2.0, 'density': 1, 'velx': 0.0, 'vely': 0.0, 'traces': []}
                         self.planets.append(newPlanet)
-                        planetEditing = self.planets.index(newPlanet)
-                        planetGrowing = True
+                        self.planetEditing = self.planets.index(newPlanet)
+                        self.planetGrowing = True
 
                 elif event.type == MOUSEBUTTONUP:
-                    if planetGrowing == True:   #Planet finished growing, now waiting for click to give it speed
-                        planetGrowing = False
+                    if self.planetGrowing == True:   #Planet finished growing, now waiting for click to give it speed
+                        self.planetGrowing = False
 
                     else:   #Click to give speed and direction
-                        planet = self.planets[planetEditing]
+                        planet = self.planets[self.planetEditing]
                         dX, dY = planet['center']
                         dX, dY = event.pos[0] - dX, event.pos[1] - dY
                         planet['velx'] = dX*velScale
                         planet['vely'] = dY*velScale
-                        if event.button == 3 and planetEditing != selectedPlanet:   #Right click, make vel relative to selected planet
-                            planet['velx'] += self.planets[selectedPlanet]['velx']
-                            planet['vely'] += self.planets[selectedPlanet]['vely']
+                        if event.button == 3 and self.planetEditing != self.selectedPlanet:   #Right click, make vel relative to selected planet
+                            planet['velx'] += self.planets[self.selectedPlanet]['velx']
+                            planet['vely'] += self.planets[self.selectedPlanet]['vely']
 
                         print ('velX' + str(planet['velx']))
-                        planetEditing = -1   #Next click draws new planet
+                        self.planetEditing = -1   #Next click draws new planet
 
-                elif event.type == KEYDOWN:
-                    if event.key == ord('r'):
-                        self.planets = []
-                        planetGrowing = False
-                        selectedPlanet = -1
-                        planetEditing = -1
-
-                    elif event.key == ord('c'):  #Center screen on one planet
-                        selectedPlanet += 1
-                        if selectedPlanet >= len(self.planets):
-                            selectedPlanet = 0
-
-                        if len(self.planets) > 0:
-                            x, y = self.planets[selectedPlanet]['center']
-                            self.planets[selectedPlanet]['center'] = WIDTH/2,HEIGHT/2
-                            dX, dY = self.planets[selectedPlanet]['center'][0] - x, self.planets[selectedPlanet]['center'][1] - y
-                            for planet in self.planets:
-                                i = self.planets.index(planet)
-                                if i != selectedPlanet:
-                                    planet['center'] = planet['center'][0] + dX, planet['center'][1] + dY
-                                for i in range(len(planet['traces'])):
-                                    planet['traces'][i] = planet['traces'][i][0] + dX, planet['traces'][i][1] + dY
-
-                    elif event.key == ord('s'):  #Show selected planet
-                        if showSelPlanet == True:
-                            showSelPlanet = False
-                        else:
-                            showSelPlanet = True
-
-                    elif event.key == ord('d'):  #Show selected planet
-                        if showSelPlanet == True and selectedPlanet != -1:
-                            self.planets.pop(selectedPlanet)
-                            planetEditing = -1
-                            planetGrowing = False
-                            selectedPlanet = -1
-
+                elif event.type == KEYDOWN:       
+                    if event.key == ord('c'):
+                        self.pendingAction = 'center'
+                    elif event.key == ord('s'):
+                        self.pendingAction = 'show'
+                    elif event.key == ord('d'):
+                        self.pendingAction = 'del'
+                    elif event.key == ord('r'):
+                        self.pendingAction = 'res'
                     elif event.key == ord('h'):
-                        helpShowing = True
-                        
-                        helpLines = helpString.split("\n")
-                        helpHeight = len(helpLines) * 20
-                        startx, starty = WIDTH/2, HEIGHT/2 - helpHeight/2
+                        self.pendingAction = 'help'
 
-                        for line in helpLines:
-                            textLine = font.render(line, True, (255,255,255),(0,0,0))
-                            textLineRect = textLine.get_rect()
-                            textLineRect.midtop = (startx,starty)
-                            starty += 20
-                            surface.blit(textLine, textLineRect)
+            if self.pendingAction != '':
+                if self.pendingAction == 'center':
+                    self.selectedPlanet += 1
+                    if self.selectedPlanet >= len(self.planets):
+                        self.selectedPlanet = 0
 
-                        pygame.display.update()
+                    if len(self.planets) > 0:
+                        x, y = self.planets[self.selectedPlanet]['center']
+                        self.planets[self.selectedPlanet]['center'] = self.WIDTH/2,self.HEIGHT/2
+                        dX, dY = self.planets[self.selectedPlanet]['center'][0] - x, self.planets[self.selectedPlanet]['center'][1] - y
+                        for planet in self.planets:
+                            i = self.planets.index(planet)
+                            if i != self.selectedPlanet:
+                                planet['center'] = planet['center'][0] + dX, planet['center'][1] + dY
+                            for i in range(len(planet['traces'])):
+                                planet['traces'][i] = planet['traces'][i][0] + dX, planet['traces'][i][1] + dY
+                elif self.pendingAction == 'show':
+                    if self.showSelPlanet == True:
+                        self.showSelPlanet = False
+                    else:
+                        self.showSelPlanet = True
+                elif self.pendingAction == 'del':
+                    if self.showSelPlanet == True and self.selectedPlanet != -1:
+                        self.planets.pop(self.selectedPlanet)
+                        self.planetEditing = -1
+                        self.planetGrowing = False
+                        self.selectedPlanet = -1
+                elif self.pendingAction == 'res':
+                    self.planets = []
+                    self.planetGrowing = False
+                    self.selectedPlanet = -1
+                    self.planetEditing = -1
+                    self.showSelPlanet = False
+                elif self.pendingAction == 'help':
+                    self.helpShowing = True  
+                    helpLines = helpString.split("\n")
+                    helpHeight = len(helpLines) * 20
+                    startx, starty = self.WIDTH/2, self.HEIGHT/2 - helpHeight/2
 
-                        while helpShowing:
-                            while gtk.events_pending():
-                                gtk.main_iteration()
-                            for event in pygame.event.get():
-                                if event.type == QUIT:
-                                    exit("Game end")
+                    for line in helpLines:
+                        textLine = font.render(line, True, (255,255,255),(0,0,0))
+                        textLineRect = textLine.get_rect()
+                        textLineRect.midtop = (startx,starty)
+                        starty += 20
+                        surface.blit(textLine, textLineRect)
 
-                                elif event.type == pygame.VIDEORESIZE:
-                                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
-                                    info = pygame.display.Info()
-                                    WIDTH = info.current_w
-                                    HEIGHT = info.current_h
+                    pygame.display.update()
 
-                                elif event.type == KEYDOWN:
-                                    if event.key == ord('h'):
-                                        helpShowing = False
-                         
+                    while self.helpShowing:
+                        while gtk.events_pending():
+                            gtk.main_iteration()
+                        for event in pygame.event.get():
+                            if event.type == QUIT:
+                                exit("Game end")
 
+                            elif event.type == pygame.VIDEORESIZE:
+                                pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                                info = pygame.display.Info()
+                                WIDTH = info.current_w
+                                HEIGHT = info.current_h
 
-            if planetGrowing == True:
-                planet = self.planets[planetEditing]
+                            elif event.type == KEYDOWN:
+                                if event.key == ord('h'):
+                                    self.helpShowing = False
+
+                self.pendingAction = ''
+
+            if self.planetGrowing == True:
+                planet = self.planets[self.planetEditing]
                 planet['radius'] += 1.01**planet['radius']
                 if planet['radius'] > 200:
                     planet['radius'] = 2.0
 
-            if planetEditing == -1:
+
+            if self.planetEditing == -1:
                 for planet0 in self.planets:   #Calc forces for each pair, calc accel from forces, calc speed from accel
                     forceX = 0.0
                     forceY = 0.0
@@ -201,7 +211,7 @@ class PlanetsGame():
             surface.fill((0,0,0))
 
             for planet in self.planets:   #Calc new position for each planet
-                if planetEditing == -1:
+                if self.planetEditing == -1:
                     x,y = planet['center']
                     planet['center'] = (x+planet['velx']*dt, y+planet['vely']*dt)
                     planet['traces'].append((planet['center'][0], planet['center'][1]))
@@ -210,9 +220,9 @@ class PlanetsGame():
 
                 if planet['center'][0] > 10000 or planet['center'][0] < -10000 or planet['center'][1] > 10000 or planet['center'][1] < -10000:
                     self.planets.pop(self.planets.index(planet))
-                    planetEditing = -1
-                    planetGrowing = False
-                    selectedPlanet = -1
+                    self.planetEditing = -1
+                    self.planetGrowing = False
+                    self.selectedPlanet = -1
                     continue
 
                 traces = planet['traces']
@@ -220,13 +230,13 @@ class PlanetsGame():
                     if i != 0:
                         intensity = 255*i/len(traces)
                         planetColor = (intensity,intensity,intensity)
-                        if self.planets.index(planet) == selectedPlanet and showSelPlanet == True:
+                        if self.planets.index(planet) == self.selectedPlanet and self.showSelPlanet == True:
                             planetColor = (0,intensity,0)
                         pygame.draw.aaline(surface,planetColor,traces[i], traces[i-1])
 
             for planet in self.planets:
                 planetColor = (255,255,255)
-                if self.planets.index(planet) == selectedPlanet and showSelPlanet == True:
+                if self.planets.index(planet) == self.selectedPlanet and self.showSelPlanet == True:
                     planetColor = (128,255,128)
 
                 #pygame.draw.circle(surface, (255,255,255), (int(planet['center'][0]), int(planet['center'][1])), planet['radius'])
@@ -234,24 +244,44 @@ class PlanetsGame():
 
             for planet in self.planets:
                 planetColor = (255,255,255)
-                if self.planets.index(planet) == selectedPlanet and showSelPlanet == True:
+                if self.planets.index(planet) == self.selectedPlanet and self.showSelPlanet == True:
                     planetColor = (128,255,128)
 
                 pygame.gfxdraw.filled_circle(surface, int(planet['center'][0]), int(planet['center'][1]), int(planet['radius']), planetColor)
 
-            surface.blit(text, textRect)
-
             pygame.display.update()
             fpsClock.tick(FPS)
 
+    def center_button_pressed (self, button):
+        self.pendingAction = 'center'
+
+    def show_button_pressed (self, button):
+        self.pendingAction = 'show'
+
+    def del_button_pressed (self, button):
+        self.pendingAction = 'del'
+
+    def restart_button_pressed (self, button):
+        self.pendingAction = 'res'
+
+    def help_button_pressed (self, button):
+        self.pendingAction = 'help'
+        self.helpShowing = False
+
+    def pause_button_pressed (self, button):
+        self.paused = not self.paused
+
     # Called to save the state of the game to the Journal.
     def write_file(self, file_path):
-        pass   #TO-DO: Save to journal
+        f = open(file_path, 'w')
+        pickle.dump(self.planets, f)
+        f.close()
 
     # Called to load the state of the game from the Journal.
     def read_file(self, file_path):
-        pass
-
+        f = open(file_path, 'r')
+        self.planets = pickle.load(f)
+        f.close()
 
 if __name__ == "__main__":
     PlanetsGame()
